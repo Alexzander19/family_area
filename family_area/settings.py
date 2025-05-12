ba3cf7f723c3ffc8b868385a81aa561839742926
Alexzander19 <2961781@mail.ru>
2025-05-12 21:34:58 +0700

    dotenv

--- family_area/settings.py
+++ family_area/settings.py
@@ -15,15 +15,17 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv  # Для .env файла (опционально)
 
-
-
-SECRET_KEY = os.getenv('SECRET_KEY')  
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))  # Загружает переменные из .env
+
+SECRET_KEY = os.getenv('SECRET_KEY')  
+
+
+
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
