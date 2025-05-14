b1ecfb4d1aacd07563db3fd8a31462b8b9a2d17b
Alexzander19 <2961781@mail.ru>
2025-05-14 19:54:05 +0700

    Test commit

--- family_area/settings.py
+++ family_area/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))  # Загружает переменные из .env
 
+#test
 SECRET_KEY = os.getenv('SECRET_KEY')  
 
 
